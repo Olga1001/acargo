4026579a2102f8a5e4982321bdfb6adaadc195d9
Olga <zhuravel.olga95@gmail.com>
2020-09-23 09:02:16 +0200

    added input in table on shipment-booking-detail page

--- app/js/admin.js
+++ app/js/admin.js
@@ -283,7 +283,7 @@ $(document).ready(function () {
     $(this).closest(".table__order-list").find(".last").toggleClass('active-scale').closest(".table__order-list").siblings().find(".last").removeClass('active-scale');
     
   });
-  var $inputs = $('.table__rate-item'); // Resize based on text if text.length > 0
+  var $inputs = $('.table__rate-item, .block_resize-input'); // Resize based on text if text.length > 0
   // Otherwise resize based on the placeholder
 
   function resizeForText(text) {
